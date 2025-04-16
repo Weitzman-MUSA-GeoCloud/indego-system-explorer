9037f273cd677bd4cd5ae3b85668cff070561da7
Mjumbe Poe <mjumbewu@gmail.com>
2025-04-16 17:24:23 -0400

    Set a relative base path in the vite config
    By default, Vite will use an absolute root path. This will cause problems on GitHub Pages.

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    build: {
-      target: 'esnext' //browsers can handle the latest ES features
-    }
-  });
+  base: './',
+  build: {
+    target: 'esnext' //browsers can handle the latest ES features
+  }
+});
